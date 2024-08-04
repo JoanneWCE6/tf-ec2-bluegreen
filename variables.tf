02efc70178c39ec7de75072e7ff37344e70024b0
Joanne Wang <joannewang@Joannes-MacBook-Air.local>
2024-08-04 15:13:24 +0800

    modified back to blue

--- variables.tf
+++ variables.tf
@@ -13,41 +13,41 @@ variable "vpc_id" {
 variable "enable_blue_env" {
   description = "Enable blue environment"
   type        = bool
-  default     = false
-}
-
-variable "enable_green_env" {
-  description = "Enable green environment"
-  type        = bool
   default     = true
 }
 
-variable "traffic_distribution" {
-  description = "Levels of traffic distribution"
-  type        = string
-}
+# variable "enable_green_env" {
+#   description = "Enable green environment"
+#   type        = bool
+#   default     = false
+# }
 
-locals {
-  traffic_dist_map = {
-    blue = {
-      blue  = 100
-      green = 0
-    }
-    blue-90 = {
-      blue  = 90
-      green = 10
-    }
-    split = {
-      blue  = 50
-      green = 50
-    }
-    green-90 = {
-      blue  = 10
-      green = 90
-    }
-    green = {
-      blue  = 0
-      green = 100
-    }
-  }
-}
+# variable "traffic_distribution" {
+#   description = "Levels of traffic distribution"
+#   type        = string
+# }
+
+# locals {
+#   traffic_dist_map = {
+#     blue = {
+#       blue  = 100
+#       green = 0
+#     }
+#     blue-90 = {
+#       blue  = 90
+#       green = 10
+#     }
+#     split = {
+#       blue  = 50
+#       green = 50
+#     }
+#     green-90 = {
+#       blue  = 10
+#       green = 90
+#     }
+#     green = {
+#       blue  = 0
+#       green = 100
+#     }
+#   }
+# }
